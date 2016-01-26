7ff7ff8d2474afcf453feaa0cfd75640992a0167
Ivan Ilves <ivan.ilves@redbooth.com>
2016-01-26 02:37:13 +0100

    Better UX (JS validations)

---
+++ public/javascripts/form_validate.js
@@ -0,0 +1,20 @@
+var targetAddressField = document.getElementById('target_address');
+var validTargetAddressRegex = /^[a-z0-9][a-zA-Z0-9\.\-]*$/;
+var targetAddressValidBackgroundColor = targetAddressField.style.backgroundColor;
+var targetAddressInvalidBackgroundColor = 'pink';
+
+function isTargetAddressValid() {
+  return !!targetAddressField.value.match(validTargetAddressRegex);
+}
+
+function validateTargetAddress() {
+  if (isTargetAddressValid()) {
+    targetAddressField.style.backgroundColor = targetAddressValidBackgroundColor;
+  } else {
+    targetAddressField.style.backgroundColor = targetAddressInvalidBackgroundColor;
+  }
+}
+
+function validateForm() {
+  return  isTargetAddressValid();
+}
