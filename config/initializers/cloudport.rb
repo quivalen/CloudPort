5852793fe2cd41dc3c48cda7ac6e86f6437be1c9
Ivan Ilves <ivan.ilves@redbooth.com>
2016-02-20 22:02:26 +0100

    Skip most of CloudPort initializer while running rake tasks

--- config/initializers/cloudport.rb
+++ config/initializers/cloudport.rb
@@ -1,4 +1,12 @@
 #
+# We should know, if we run rake task.
+# HINT: We should skip running most of this initializer code, if we run rake!
+#
+def running_rake?
+  File.basename($0) == 'rake'
+end
+
+#
 # Ensure CloudPort build root path created
 #
 unless Dir.exist?(CloudPort::Application.config.build_root)
@@ -8,31 +16,13 @@ end
 #
 # Ensure required Docker image pulled
 #
-begin
-  Docker::Image.create(
-    'fromImage' => CloudPort::Application.config.docker_image
-  )
-rescue => e
-  Rails.logger.fatal("Failed to import Docker image: #{e.message}")
+unless running_rake?
+  Docker::Image.create('fromImage' => CloudPort::Application.config.docker_image)
 end
 
 #
 # Start Docker containers for all builds
 #
-# Note: We check for `builds` table existence,
-# cause we don't want to fail 'db:schema:load'
-# during initial provisioning of application!
-#
-if ActiveRecord::Base.connection.table_exists?(:builds)
-  begin
-    Build.all.each do |b|
-      begin
-        b.docker_container.start
-      rescue => e
-        Rails.logger.error("Failed to start container: #{e.message}")
-      end
-    end
-  rescue => e
-    rails.logger.fatal("Failed to initialize containers: #{e.message}")
-  end
+unless running_rake?
+  Build.all.each { |b| b.docker_container.start }
 end
