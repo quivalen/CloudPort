9a93714c6c522705525a07d17fab2474a989d7e6
Ivan Ilves <ivan.ilves@redbooth.com>
2016-03-05 15:43:18 +0100

    Decouple Docker subsystem from initializer

--- config/initializers/cloudport.rb
+++ config/initializers/cloudport.rb
@@ -1,16 +1,4 @@
 #
-# We should know, if we run rake task, scheduler etc.
-# HINT: We should skip running most of this initializer code, if we run "em!
-#
-def running_rake?
-  File.basename($0) == 'rake'
-end
-
-def skip_docker?
-  !!ENV['SKIP_DOCKER']
-end
-
-#
 # Ensure CloudPort build root path created
 #
 unless Dir.exist?(CloudPort::Application.config.build_root)
@@ -18,15 +6,8 @@ unless Dir.exist?(CloudPort::Application.config.build_root)
 end
 
 #
-# Ensure required Docker image pulled
+# Start [Docker] containers, if ordered to
 #
-unless running_rake? || skip_docker?
-  Docker::Image.create('fromImage' => CloudPort::Application.config.docker_image)
-end
-
-#
-# Start Docker containers for all builds
-#
-unless running_rake? || skip_docker?
+if ENV['START_CONTAINERS']
   Container.all.each { |c| c.docker_container.start }
 end
