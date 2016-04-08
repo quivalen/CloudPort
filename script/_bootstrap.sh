ec432594341105054d397771ce0ed6eea394483a
Ivan Ilves <ivan.ilves@redbooth.com>
2016-04-08 14:42:23 +0200

    You should run it on Linux! REALLY ;)

--- script/_bootstrap.sh
+++ script/_bootstrap.sh
@@ -23,6 +23,7 @@ function install_ruby() {
 }
 
 if [[ "${DEPLOY_ENV}" == "local" ]]; then
+  [[ "$(uname -s)" == "Linux" ]] || die 'You should run it on Linux!'
   [[ -x "$(which apt-get)" ]] || die 'Apt-Get executable not found!'
   [[ "${LINUX_DISTRO}" == "Ubuntu" ]] || die 'Only supported distro is Ubuntu, sorry ;('
   [[ "${MACHINE_TYPE}" == "Virtual Machine" ]] || die 'Should be deployed only on virtual machines!'
