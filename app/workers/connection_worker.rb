9a896160d8cc5f513702003adc4717409e609160
Ivan Ilves <ivan.ilves@redbooth.com>
2016-04-28 00:22:24 +0200

    Protect ConnectionWorker from exceptions

--- app/workers/connection_worker.rb
+++ app/workers/connection_worker.rb
@@ -7,7 +7,11 @@ class ConnectionWorker
 
   def perform
     Container.all.each do |c|
-      c.synchronize_connections!
+      begin
+        c.synchronize_connections!
+      rescue => e
+        Rails.logger.error("Docker container #{c.docker_container_id}: #{e.message}")
+      end
     end
   end
 
