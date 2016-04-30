5e9d04356f80eec97282f8f44a7a485881e85eb9
Ivan Ilves <ivan.ilves@redbooth.com>
2016-04-30 18:31:54 +0200

    Better `synchronize_connections!` exception message

--- app/workers/connection_worker.rb
+++ app/workers/connection_worker.rb
@@ -10,7 +10,11 @@ class ConnectionWorker
       begin
         c.synchronize_connections!
       rescue => e
-        Rails.logger.error("Docker container #{c.docker_container_id}: #{e.message}")
+        Rails.logger.error("\
+          Failed to synchronize connections state \
+          for Docker container [#{c.docker_container_id}]:\n\
+          #{e.message}\
+        ")
       end
     end
   end
